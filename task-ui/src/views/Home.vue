9bf2740483f30cab05f13d355cc2699db817eade
Dee2311 <deepak2311parmar@gmail.com>
2020-01-31 09:57:10 +0530

    creating vue project

---
+++ task-ui/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+     <HelloWorld msg="Welcome to Your Task Tracker App" />
+    <div class="bg">
+      <button class="start">Let's Start</button>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+<style scoped>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.bg {
+  /* The image used */
+  background-image: url("../assets/bg.jpg");
+
+  /* Full height */
+  height: 100%; 
+  width: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
